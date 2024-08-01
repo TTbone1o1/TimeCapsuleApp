b7be16372afaeaffc4edcbdb95b5ed2ecd7544ec
Abraham May <Abrahammay1015@gmail.com>
2024-08-01 16:51:07 -0500

    Initial Commit

---
+++ TimeCapsuleApp/TimeCapsuleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCapsuleAppApp.swift
+//  TimeCapsuleApp
+//
+//  Created by Abraham May on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeCapsuleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
