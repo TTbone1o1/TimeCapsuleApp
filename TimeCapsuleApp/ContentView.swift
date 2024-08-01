bb37e4db2e07efc8be1b4e59a877f1629932e67d
Abraham May <Abrahammay1015@gmail.com>
2024-08-01 17:40:50 -0500

    For now just trying to get the base of the profile page might

--- TimeCapsuleApp/ContentView.swift
+++ TimeCapsuleApp/ContentView.swift
@@ -9,11 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack() {
+            Rectangle()
+                .frame(width: 361, height: 410)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .cornerRadius(40)
+            
+            Rectangle()
+                .frame(width: 325, height: 62)
+                .cornerRadius(50)
+            
+            HStack{
+                Rectangle()
+                    .frame(width: 140, height: 140)
+                    .foregroundColor(.white)
+                    .cornerRadius(6)
+                    .shadow(radius: 24, x: 0, y: 7)
+                
+                Rectangle()
+                    .frame(width: 140, height: 140)
+                    .foregroundColor(.white)
+                    .cornerRadius(6)
+                    .shadow(radius: 24, x: 0, y: 7)
+            }
+            
+            HStack{
+                Rectangle()
+                    .frame(width: 140, height: 140)
+                    .foregroundColor(.white)
+                    .cornerRadius(6)
+                    .shadow(radius: 24, x: 0, y: 7)
+                
+                Rectangle()
+                    .frame(width: 140, height: 140)
+                    .foregroundColor(.white)
+                    .cornerRadius(6)
+                    .shadow(radius: 24, x: 0, y: 7)
+            }
         }
         .padding()
     }
