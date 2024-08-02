3c35f33c3b4b01fadbb483c32ab9d131a1b25bdb
Abraham May <Abrahammay1015@gmail.com>
2024-08-02 12:36:35 -0500

    Added a stroke and replace the images with other ones

--- TimeCapsuleApp/Timecap.swift
+++ TimeCapsuleApp/Timecap.swift
@@ -20,30 +20,42 @@ struct Timecap: View {
             
             HStack {
                 Spacer()
-                HStack(spacing: -50){
+                HStack(){
                     
                     Image("1")
                         .resizable()
                         .frame(width: 116, height: 169.35)
                         .cornerRadius(19)
-                        .zIndex(3)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 19)
+                                .stroke(Color.white, lineWidth: 4))
                         .rotationEffect(Angle(degrees: -16))
-                        .offset(y: 10)
+                        .offset(x: 25, y: 15)
+                        .shadow(radius: 24, x: 0, y: 14)
+                        .zIndex(3)
                     
                     Image("2")
                         .resizable()
                         .frame(width: 116, height: 169.35)
                         .cornerRadius(19)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 19)
+                                .stroke(Color.white, lineWidth: 4))
                         .zIndex(2)
                         .rotationEffect(Angle(degrees: -2))
+                        .shadow(radius: 24, x: 0, y: 14)
                     
                     Image("3")
                         .resizable()
                         .frame(width: 116, height: 169.35)
                         .cornerRadius(19)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 19)
+                                .stroke(Color.white, lineWidth: 4))
                         .zIndex(1)
                         .rotationEffect(Angle(degrees: 17))
-                        .offset(y: 10)
+                        .shadow(radius: 24, x: 0, y: 14)
+                        .offset(x: -33, y: 15)
                     
                 }
                 .frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .top)
