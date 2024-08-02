8fae182d17206cb090fd583d6587bf04901d36ae
Abraham May <Abrahammay1015@gmail.com>
2024-08-02 10:01:17 -0500

    Created a new file

---
+++ TimeCapsuleApp/Timecap.swift
@@ -0,0 +1,18 @@
+//
+//  Timecap.swift
+//  TimeCapsuleApp
+//
+//  Created by Abraham May on 8/2/24.
+//
+
+import SwiftUI
+
+struct Timecap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Timecap()
+}
