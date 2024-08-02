07007490d6f77a5b3a83d33555ebb2f63bf075d4
Abraham May <Abrahammay1015@gmail.com>
2024-08-02 14:31:03 -0500

    Created a new file

---
+++ TimeCapsuleApp/Create.swift
@@ -0,0 +1,18 @@
+//
+//  Create.swift
+//  TimeCapsuleApp
+//
+//  Created by Abraham May on 8/2/24.
+//
+
+import SwiftUI
+
+struct Create: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Create()
+}
