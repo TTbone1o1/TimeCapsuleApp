1543e2becea113a80f942c952628b2e0e4a3c5d4
Abraham May <Abrahammay1015@gmail.com>
2024-08-02 15:38:38 -0500

    Created the create username page

--- TimeCapsuleApp/Create.swift
+++ TimeCapsuleApp/Create.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct Create: View {
+    
+    @State private var username: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack{
+                Rectangle()
+                    .frame(width: 250, height: 65)
+                    .cornerRadius(11)
+                    .foregroundColor(.gray)
+                    .shadow(radius: 24, x: 0, y: 14)
+                
+                HStack {
+                    ZStack(alignment: .leading) {
+                        if username.isEmpty {
+                            Text("Create a username")
+                                .foregroundColor(.white)
+                                .font(.system(size: 16, weight: .semibold))
+                                .padding(.leading, 50)
+                        }
+                        TextField("", text: $username)
+                            .foregroundColor(.white)
+                            .font(.system(size: 16, weight: .semibold))
+                            .padding(.leading, 10)
+                    }
+                }
+            }
+        }
+        .frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .top)
+        .padding(.top, 311)
+        .padding(.horizontal, 200)
+        
+        Button(action: {
+            //action for button
+            print("hi")
+        }, label: {
+            ZStack{
+                Rectangle()
+                    .frame(width: 291, height: 62)
+                    .cornerRadius(40)
+                    .foregroundColor(.black)
+                    .shadow(radius: 24, x: 0, y: 14)
+                
+                HStack {
+                    
+                    Text("Continue")
+                        .foregroundColor(.white)
+                    .font(.system(size: 16, weight: .semibold))
+                }
+            }
+        })
+        .padding(.bottom, 20)
     }
 }
 
