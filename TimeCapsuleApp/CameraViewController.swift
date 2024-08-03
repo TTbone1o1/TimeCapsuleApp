463ab328492bd207bced5cd3c28c109e3a19a068
Abraham May <Abrahammay1015@gmail.com>
2024-08-03 09:53:11 -0500

    Added the functionality for the camera

--- TimeCapsuleApp/CameraViewController.swift
+++ TimeCapsuleApp/CameraViewController.swift
@@ -1,18 +1,41 @@
-//
-//  CameraViewController.swift
-//  TimeCapsuleApp
-//
-//  Created by Abraham May on 8/3/24.
-//
-
 import SwiftUI
+import UIKit
 
-struct CameraViewController: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct CameraViewController: UIViewControllerRepresentable {
+    @Binding var isShown: Bool
+    @Binding var image: Image?
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        @Binding var isShown: Bool
+        @Binding var image: Image?
+
+        init(isShown: Binding<Bool>, image: Binding<Image?>) {
+            _isShown = isShown
+            _image = image
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                image = Image(uiImage: uiImage)
+            }
+            isShown = false
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            isShown = false
+        }
     }
-}
 
-#Preview {
-    CameraViewController()
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(isShown: $isShown, image: $image)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<CameraViewController>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .camera
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<CameraViewController>) {}
 }
