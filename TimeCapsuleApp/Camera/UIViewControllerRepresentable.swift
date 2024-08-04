a1e864b99b85b07ab1fcb2fd2c5244a442df703c
Abraham May <Abrahammay1015@gmail.com>
2024-08-04 17:33:07 -0500

    For now commeted the code in this file not much use anymore for now

--- TimeCapsuleApp/Camera/UIViewControllerRepresentable.swift
+++ TimeCapsuleApp/Camera/UIViewControllerRepresentable.swift
@@ -1,15 +1,15 @@
-import SwiftUI
-
-struct CameraViewControllerRepresentable: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> Camera {
-        Camera() // Instantiate your Camera view controller
-    }
-    
-    func updateUIViewController(_ uiViewController: Camera, context: Context) {
-        // Update the view controller if needed
-    }
-}
-
-#Preview {
-    CameraViewControllerRepresentable()
-}
+//import SwiftUI
+//
+//struct CameraViewControllerRepresentable: UIViewControllerRepresentable {
+//    func makeUIViewController(context: Context) -> Camera {
+//        Camera() // Instantiate your Camera view controller
+//    }
+//    
+//    func updateUIViewController(_ uiViewController: Camera, context: Context) {
+//        // Update the view controller if needed
+//    }
+//}
+//
+//#Preview {
+//    CameraViewControllerRepresentable()
+//}
