4101fd2824c99c7e0e0273e291908be01fa27457
Abraham May <Abrahammay1015@gmail.com>
2024-08-04 09:17:31 -0500

    Edited the modalview to have the same button style

--- TimeCapsuleApp/ModalView.swift
+++ TimeCapsuleApp/ModalView.swift
@@ -16,24 +16,25 @@ struct Modal: View {
                 TextField("Enter your activity", text: $userInput)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding(.horizontal, 20)
-                Button(action: {
-                    onSubmit()
-                }) {
-                    Text("Submit")
-                        .bold()
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                        .padding(.horizontal, 20)
-                        .padding(.bottom, 20)
+                ZStack {
+                    Rectangle()
+                        .frame(width: 291, height: 62)
+                        .cornerRadius(40)
+                        .foregroundColor(.black)
+                        .shadow(radius: 24, x: 0, y: 14)
+                    
+                    HStack {
+                        Text("Continue")
+                            .foregroundColor(.white)
+                            .font(.system(size: 16, weight: .semibold))
+                    }
                 }
             }
             .background(Color.white)
             .cornerRadius(20)
             .shadow(radius: 10)
             .frame(height: 315)
+            .padding(.bottom, -60)
         }
         .edgesIgnoringSafeArea(.all)
     }
