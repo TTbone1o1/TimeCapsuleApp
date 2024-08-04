8267fa0c10868598859721602ce71b1305eaeb3f
Abraham May <Abrahammay1015@gmail.com>
2024-08-04 15:28:15 -0500

    For now commented out MainView no reason to have it for now but maybe save it for later

--- TimeCapsuleApp/MainView.swift
+++ TimeCapsuleApp/MainView.swift
@@ -1,8 +1,27 @@
+//import SwiftUI
 //
-//  MainView.swift
-//  TimeCapsuleApp
+//struct MainView: View {
+//    @State private var showModal = true
+//    @State private var userInput = ""
 //
-//  Created by Abraham May on 8/4/24.
+//    var body: some View {
+//        ZStack {
+//            // Your camera view or any other content
+//            CameraViewControllerRepresentable()
+//                .opacity(showModal ? 0 : 1) // Hide camera view when modal is shown
 //
-
-import Foundation
+//            if showModal {
+//                Modal(showModal: $showModal, username: "User", userInput: $userInput) {
+//                    // Handle submission logic here
+//                    print("Submitted: \(userInput)")
+//                }
+//                .transition(.move(edge: .bottom)) // Optional: Add transition for modal dismissal
+//                .animation(.easeInOut, value: showModal) // Optional: Animate modal appearance/disappearance
+//            }
+//        }
+//    }
+//}
+//
+//#Preview {
+//    MainView()
+//}
