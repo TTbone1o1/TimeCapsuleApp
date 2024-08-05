c0298eb8181eee211d6990f48da6db1b584ac4ba
Abraham May <Abrahammay1015@gmail.com>
2024-08-05 09:53:10 -0500

    Created a new file for the photo info

---
+++ TimeCapsuleApp/Info/Writeinfo.swift
@@ -0,0 +1,18 @@
+//
+//  Writeinfo.swift
+//  TimeCapsuleApp
+//
+//  Created by Abraham May on 8/5/24.
+//
+
+import SwiftUI
+
+struct Writeinfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Writeinfo()
+}
