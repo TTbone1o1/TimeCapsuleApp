2c1417daa9d366e02b557198d632806220e34f69
Abraham May <Abrahammay1015@gmail.com>
2024-08-05 15:48:44 -0500

    Styled a couple things in Photoinfo.swift

---
+++ TimeCapsuleApp/Color.swift
@@ -0,0 +1,18 @@
+//
+//  Color.swift
+//  TimeCapsuleApp
+//
+//  Created by Abraham May on 8/5/24.
+//
+
+import SwiftUI
+
+struct Color: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Color()
+}
--- TimeCapsuleApp/Info/Photoinfo.swift
+++ TimeCapsuleApp/Info/Photoinfo.swift
@@ -2,14 +2,14 @@ import SwiftUI
 
 struct Photoinfo: View {
     var body: some View {
+        NavigationView {
         VStack {
             Spacer()
                 .frame(height: 100) // Pushes the content down by 130 points from the top
-
+            
             Text("Take one photo daily")
                 .font(.system(size: 24))
                 .fontWeight(.bold)
-
             Spacer()
             
             HStack {
@@ -35,25 +35,24 @@ struct Photoinfo: View {
             }
             
             Spacer() // Pushes the button towards the bottom
-
-            Button(action: {
-                // for now it's just a button
-            }) {
-                ZStack {
-                    Rectangle()
-                        .frame(width: 291, height: 62)
-                        .cornerRadius(40)
-                        .foregroundColor(.black)
-                        .shadow(radius: 24, x: 0, y: 14)
-
-                    HStack {
-                        Text("Continue")
-                            .foregroundColor(.white)
-                            .font(.system(size: 16, weight: .semibold))
+            
+                NavigationLink(destination: Writeinfo()) {
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 291, height: 62)
+                            .cornerRadius(40)
+                            .foregroundColor(.black)
+                            .shadow(radius: 24, x: 0, y: 14)
+                        
+                        HStack {
+                            Text("Continue")
+                                .foregroundColor(.white)
+                                .font(.system(size: 16, weight: .semibold))
+                        }
                     }
                 }
+                .padding(.bottom, 20) // Adds some space at the bottom of the screen
             }
-            .padding(.bottom, 20) // Adds some space at the bottom of the screen
         }
         .frame(maxHeight: .infinity) // Ensures the VStack takes the full height of the screen
     }
