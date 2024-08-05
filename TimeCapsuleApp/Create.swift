57c235fd85ab932e884d93829830102a47deaa47
Abraham May <Abrahammay1015@gmail.com>
2024-08-05 14:53:58 -0500

    Edited the create styling a bit

--- TimeCapsuleApp/Create.swift
+++ TimeCapsuleApp/Create.swift
@@ -37,28 +37,28 @@ struct Create: View {
             
             ZStack {
                 Rectangle()
-                    .frame(width: 250, height: 65)
-                    .cornerRadius(11)
-                    .foregroundColor(.gray)
-                    .shadow(radius: 24, x: 0, y: 14)
+                    .fill(Color(hex: "#EDEDED"))
+                    .frame(width: 270, height: 80) // Keep the original size for the box
+                    .cornerRadius(20)
                 
                 HStack {
                     ZStack(alignment: .leading) {
                         if username.isEmpty {
                             Text("@username")
-                                .foregroundColor(.white) // Set the placeholder color here
-                                .font(.system(size: 16, weight: .semibold))
-                                .padding(.leading, 10)
+                                .foregroundColor(.gray) // Set the placeholder color here
+                                .frame(width: 270, height: 60) // Same width and height as the box
+                                .cornerRadius(20) // Rounded corners for the text field
+                                .fontWeight(.bold)
                         }
                         TextField("", text: $username)
-                            .foregroundColor(.white) // Set the text color here
-                            .font(.system(size: 16, weight: .semibold))
-                            .padding(.horizontal, 10)
+                            .foregroundColor(.gray) // Set the text color here
+                            .font(.system(size: 28, weight: .bold))
+                            
                             .frame(maxWidth: .infinity) // Expands to fill available width
                             .textFieldStyle(PlainTextFieldStyle())
                             .multilineTextAlignment(.center)
                     }
-                    .padding(.horizontal, 140) // Adjust padding to center the text field
+                    .padding(.horizontal, 90) // Adjust padding to center the text field
                 }
             }
             .frame(maxHeight: .infinity, alignment: .top)
@@ -97,6 +97,20 @@ struct Create: View {
 //        }
 //    }
 
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.scanLocation = 1 // Bypass the '#' character
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+        
+        let red = Double((rgb >> 16) & 0xFF) / 255.0
+        let green = Double((rgb >> 8) & 0xFF) / 255.0
+        let blue = Double(rgb & 0xFF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue)
+    }
+}
 
 #Preview {
     Create()
