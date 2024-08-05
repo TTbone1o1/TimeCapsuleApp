d51f7319458278ac9e23f3a3e7f6e2586677863a
Abraham May <Abrahammay1015@gmail.com>
2024-08-05 16:02:35 -0500

    Fixed the back button issue

--- TimeCapsuleApp/Info/Photoinfo.swift
+++ TimeCapsuleApp/Info/Photoinfo.swift
@@ -3,40 +3,38 @@ import SwiftUI
 struct Photoinfo: View {
     var body: some View {
         NavigationView {
-        VStack {
-            Spacer()
-                .frame(height: 100) // Pushes the content down by 130 points from the top
-            
-            Text("Take one photo daily")
-                .font(.system(size: 24))
-                .fontWeight(.bold)
-            Spacer()
-            
-            HStack {
+            VStack {
+                Spacer()
+                    .frame(height: 100) // Pushes the content down by 100 points from the top
+                
+                Text("Take one photo daily")
+                    .font(.system(size: 24))
+                    .fontWeight(.bold)
+                Spacer()
                 
                 HStack {
-                    Image("2")
-                        .resizable()
-                        .frame(width: 217, height: 328)
-                        .cornerRadius(19)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 19)
-                                .stroke(Color.white, lineWidth: 4)
-                        )
-                        .shadow(radius: 24, x: 0, y: 14)
-                    
-                        .overlay(
-                            Circle()
-                                .foregroundColor(.white)
-                                .frame(width: 38, height: 38)
-                                .offset(y: 130)
-                        )
+                    HStack {
+                        Image("2")
+                            .resizable()
+                            .frame(width: 217, height: 328)
+                            .cornerRadius(19)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 19)
+                                    .stroke(Color.white, lineWidth: 4)
+                            )
+                            .shadow(radius: 24, x: 0, y: 14)
+                            .overlay(
+                                Circle()
+                                    .foregroundColor(.white)
+                                    .frame(width: 38, height: 38)
+                                    .offset(y: 130)
+                            )
+                    }
                 }
-            }
-            
-            Spacer() // Pushes the button towards the bottom
-            
-                NavigationLink(destination: Writeinfo()) {
+                
+                Spacer() // Pushes the button towards the bottom
+                
+                NavigationLink(destination: Writeinfo().navigationBarBackButtonHidden(true)) {
                     ZStack {
                         Rectangle()
                             .frame(width: 291, height: 62)
@@ -53,6 +51,7 @@ struct Photoinfo: View {
                 }
                 .padding(.bottom, 20) // Adds some space at the bottom of the screen
             }
+            .navigationBarBackButtonHidden(true) // Hides back button for this view
         }
         .frame(maxHeight: .infinity) // Ensures the VStack takes the full height of the screen
     }
