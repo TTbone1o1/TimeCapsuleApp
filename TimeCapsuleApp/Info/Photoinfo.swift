88cd086d1c24d5c8a83817e6ea96f9ec6c97bb4f
Abraham May <Abrahammay1015@gmail.com>
2024-08-05 10:17:28 -0500

    Added text and a continue button

--- TimeCapsuleApp/Info/Photoinfo.swift
+++ TimeCapsuleApp/Info/Photoinfo.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct Photoinfo: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Take one photo daily")
+                .font(.system(size: 24))
+                .fontWeight(.semibold)
+            
+                    Button(action: {
+                        // for now its just a button
+                    }) {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 291, height: 62)
+                                .cornerRadius(40)
+                                .foregroundColor(.black)
+                                .shadow(radius: 24, x: 0, y: 14)
+            
+                            HStack {
+                                Text("Continue")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 16, weight: .semibold))
+                            }
+                        }
+                    }
+                    .padding(.bottom, 20)
+        }
+
     }
 }
 
