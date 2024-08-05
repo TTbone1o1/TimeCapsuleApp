ff9706729270f8f298e255dc030475662bd12d25
Abraham May <Abrahammay1015@gmail.com>
2024-08-05 10:50:34 -0500

    For now copied my code from photoinfo into writeinfo

--- TimeCapsuleApp/Info/Writeinfo.swift
+++ TimeCapsuleApp/Info/Writeinfo.swift
@@ -9,7 +9,60 @@ import SwiftUI
 
 struct Writeinfo: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+                .frame(height: 100) // Pushes the content down by 130 points from the top
+
+            Text("Take one photo daily")
+                .font(.system(size: 24))
+                .fontWeight(.bold)
+
+            Spacer()
+            
+            HStack {
+                
+                HStack {
+                    Image("2")
+                        .resizable()
+                        .frame(width: 217, height: 328)
+                        .cornerRadius(19)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 19)
+                                .stroke(Color.white, lineWidth: 4)
+                        )
+                        .shadow(radius: 24, x: 0, y: 14)
+                    
+                        .overlay(
+                            Circle()
+                                .foregroundColor(.white)
+                                .frame(width: 38, height: 38)
+                                .offset(y: 130)
+                        )
+                }
+            }
+            
+            Spacer() // Pushes the button towards the bottom
+
+            Button(action: {
+                // for now it's just a button
+            }) {
+                ZStack {
+                    Rectangle()
+                        .frame(width: 291, height: 62)
+                        .cornerRadius(40)
+                        .foregroundColor(.black)
+                        .shadow(radius: 24, x: 0, y: 14)
+
+                    HStack {
+                        Text("Continue")
+                            .foregroundColor(.white)
+                            .font(.system(size: 16, weight: .semibold))
+                    }
+                }
+            }
+            .padding(.bottom, 20) // Adds some space at the bottom of the screen
+        }
+        .frame(maxHeight: .infinity)
     }
 }
 
