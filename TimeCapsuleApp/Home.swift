dd3921273803b26002d0e65ece625a5c3ca310cf
Abraham May <Abrahammay1015@gmail.com>
2024-08-06 11:58:32 -0500

    Gave the shutterbutton a navagation link to camera.swift

--- TimeCapsuleApp/Home.swift
+++ TimeCapsuleApp/Home.swift
@@ -5,142 +5,145 @@ struct Home: View {
     @State private var imagesAppeared = false
     
     var body: some View {
-        VStack {
-            HStack {
-                Text(username.isEmpty ? "No Username" : username) // Display the username
-                    .font(.system(size: 18))
-                    .fontWeight(.bold)
-                    .padding()
-                
-                Spacer() // Pushes the VStack to the right
-                
-                Button(action: {
+        NavigationView {
+            VStack {
+                HStack {
+                    Text(username.isEmpty ? "No Username" : username) // Display the username
+                        .font(.system(size: 18))
+                        .fontWeight(.bold)
+                        .padding()
                     
-                }, label: {
+                    Spacer() // Pushes the VStack to the right
                     
-                    VStack(spacing: 2) {
-                        ForEach(0..<3) { _ in
-                            Rectangle()
-                                .frame(width: 16, height: 3)
-                                .cornerRadius(20)
-                                .foregroundColor(.black) // Change color as needed
+                    Button(action: {
+                        
+                    }, label: {
+                        
+                        VStack(spacing: 2) {
+                            ForEach(0..<3) { _ in
+                                Rectangle()
+                                    .frame(width: 16, height: 3)
+                                    .cornerRadius(20)
+                                    .foregroundColor(.black) // Change color as needed
+                            }
                         }
+                        .padding(.trailing)
+                    })
+                }
+                
+                Spacer()
+                
+                Text("Take a photo to start")
+                    .font(.system(size: 18))
+                    .padding(.bottom, 30)
+                    .fontWeight(.bold)
+                
+                HStack {
+                    Spacer()
+                    
+                    HStack {
+                        Image("1")
+                            .resizable()
+                            .frame(width: 82.37, height: 120.26)
+                            .cornerRadius(19)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 19)
+                                    .stroke(Color.white, lineWidth: 4)
+                            )
+                            .rotationEffect(Angle(degrees: -16))
+                            .offset(x: 25, y: 15)
+                            .shadow(radius: 24, x: 0, y: 14)
+                            .zIndex(3)
+                            .scaleEffect(imagesAppeared ? 1 : 0)
+                            .animation(.interpolatingSpring(stiffness: 60, damping: 7).delay(0.1), value: imagesAppeared)
+                            .onAppear {
+                                if imagesAppeared {
+                                    triggerHaptic()
+                                }
+                            }
+                        
+                        Image("2")
+                            .resizable()
+                            .frame(width: 82.37, height: 120.26)
+                            .cornerRadius(19)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 19)
+                                    .stroke(Color.white, lineWidth: 4)
+                            )
+                            .zIndex(2)
+                            .rotationEffect(Angle(degrees: -2))
+                            .shadow(radius: 24, x: 0, y: 14)
+                            .scaleEffect(imagesAppeared ? 1 : 0)
+                            .animation(.interpolatingSpring(stiffness: 60, damping: 7).delay(0.2), value: imagesAppeared)
+                            .onAppear {
+                                if imagesAppeared {
+                                    triggerHaptic()
+                                }
+                            }
+                        
+                        Image("3")
+                            .resizable()
+                            .frame(width: 82.37, height: 120.26)
+                            .cornerRadius(19)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 19)
+                                    .stroke(Color.white, lineWidth: 4)
+                            )
+                            .zIndex(1)
+                            .rotationEffect(Angle(degrees: 17))
+                            .shadow(radius: 24, x: 0, y: 14)
+                            .offset(x: -33, y: 15)
+                            .scaleEffect(imagesAppeared ? 1 : 0)
+                            .animation(.interpolatingSpring(stiffness: 60, damping: 7).delay(0.3), value: imagesAppeared)
+                            .onAppear {
+                                if imagesAppeared {
+                                    triggerHaptic()
+                                }
+                            }
                     }
-                    .padding(.trailing)
-                })
-            }
-
-            Spacer()
-            
-            Text("Take a photo to start")
-                .font(.system(size: 18))
-                .padding(.bottom, 30)
-                .fontWeight(.bold)
-            
-            HStack {
+                    Spacer()
+                }
+                
                 Spacer()
                 
                 HStack {
-                    Image("1")
-                        .resizable()
-                        .frame(width: 82.37, height: 120.26)
-                        .cornerRadius(19)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 19)
-                                .stroke(Color.white, lineWidth: 4)
-                        )
-                        .rotationEffect(Angle(degrees: -16))
-                        .offset(x: 25, y: 15)
-                        .shadow(radius: 24, x: 0, y: 14)
-                        .zIndex(3)
-                        .scaleEffect(imagesAppeared ? 1 : 0)
-                        .animation(.interpolatingSpring(stiffness: 60, damping: 7).delay(0.1), value: imagesAppeared)
-                        .onAppear {
-                            if imagesAppeared {
-                                triggerHaptic()
-                            }
+                    NavigationLink(destination: CameraView()
+                                   //gets rid of the border UIViewControllerRepresentable creates for camera
+                        .edgesIgnoringSafeArea(.all)
+                    ) {
+                        ZStack {
+                            Circle()
+                                .stroke(Color.gray, lineWidth: 3) // Added white stroke
+                                .frame(width: 24, height: 24)
+                            
+                            Circle()
+                                .frame(width: 13, height: 13)
+                                .foregroundColor(.gray) // Change color as needed
                         }
-                    
-                    Image("2")
-                        .resizable()
-                        .frame(width: 82.37, height: 120.26)
-                        .cornerRadius(19)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 19)
-                                .stroke(Color.white, lineWidth: 4)
-                        )
-                        .zIndex(2)
-                        .rotationEffect(Angle(degrees: -2))
-                        .shadow(radius: 24, x: 0, y: 14)
-                        .scaleEffect(imagesAppeared ? 1 : 0)
-                        .animation(.interpolatingSpring(stiffness: 60, damping: 7).delay(0.2), value: imagesAppeared)
-                        .onAppear {
-                            if imagesAppeared {
-                                triggerHaptic()
-                            }
-                        }
-                    
-                    Image("3")
-                        .resizable()
-                        .frame(width: 82.37, height: 120.26)
-                        .cornerRadius(19)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 19)
-                                .stroke(Color.white, lineWidth: 4)
-                        )
-                        .zIndex(1)
-                        .rotationEffect(Angle(degrees: 17))
-                        .shadow(radius: 24, x: 0, y: 14)
-                        .offset(x: -33, y: 15)
-                        .scaleEffect(imagesAppeared ? 1 : 0)
-                        .animation(.interpolatingSpring(stiffness: 60, damping: 7).delay(0.3), value: imagesAppeared)
-                        .onAppear {
-                            if imagesAppeared {
-                                triggerHaptic()
-                            }
-                        }
-                }
-                Spacer()
-            }
-            
-            Spacer()
-            
-            HStack {
-                Button(action: {
-                    
-                }, label: {
-                    ZStack {
-                        Circle()
-                            .stroke(Color.gray, lineWidth: 3) // Added white stroke
-                            .frame(width: 24, height: 24)
-                        
-                        Circle()
-                            .frame(width: 13, height: 13)
-                            .foregroundColor(.gray) // Change color as needed
                     }
-                })
-                
-                Spacer()
-                    .frame(width: 72)
-                
-                Button(action: {
                     
-                }, label: {
-                    Image("Notebook")
-                })
+                    Spacer()
+                        .frame(width: 72)
+                    
+                    
+                        Image("Notebook")
+                        
+                    
+                    
+                }
             }
-        }
-        .padding() // Optional: Adds padding around the VStack
-        .frame(maxHeight: .infinity) // Ensure VStack takes full height of the screen
-        .onAppear {
-            // Start the animation when the view appears
-            imagesAppeared = true
-            // Trigger haptic feedback when the view first appears
-            triggerHaptic()
-        }
-        .onDisappear {
-            // Reset the animation state if necessary
-            imagesAppeared = false
+            .padding() // Optional: Adds padding around the VStack
+            .frame(maxHeight: .infinity) // Ensure VStack takes full height of the screen
+            .onAppear {
+                // Start the animation when the view appears
+                imagesAppeared = true
+                // Trigger haptic feedback when the view first appears
+                triggerHaptic()
+            }
+            .onDisappear {
+                // Reset the animation state if necessary
+                imagesAppeared = false
+            }
         }
     }
 }
