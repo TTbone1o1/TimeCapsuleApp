7a4a08169e9a5996ad101c83dbf3833ef0033d5a
Abraham May <Abrahammay1015@gmail.com>
2024-08-07 15:58:47 -0500

    Created a new file for the post button

---
+++ TimeCapsuleApp/PostButton.swift
@@ -0,0 +1,18 @@
+//
+//  PostView.swift
+//  TimeCapsuleApp
+//
+//  Created by Abraham May on 8/7/24.
+//
+
+import SwiftUI
+
+struct PostButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostButton()
+}
