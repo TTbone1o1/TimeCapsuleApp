f9b26479d863333351273b56927cd216b760b237
Abraham May <Abrahammay1015@gmail.com>
2024-08-06 19:45:46 -0500

    Edited the controller for the camera

--- TimeCapsuleApp/Camera/CameraController.swift
+++ TimeCapsuleApp/Camera/CameraController.swift
@@ -3,14 +3,36 @@ import UIKit
 
 struct CameraView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> Camera {
-        return Camera()
+        let camera = Camera()
+        return camera
     }
 
     func updateUIViewController(_ uiViewController: Camera, context: Context) {
         // Update the view controller if needed
     }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject {
+        var parent: CameraView
+
+        init(_ parent: CameraView) {
+            self.parent = parent
+        }
+    }
 }
 
-#Preview{
-    CameraView()
+struct CameraController: View {
+    var body: some View {
+        CameraView()
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CameraController_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraController()
+    }
 }
