7ae580698c365f72b2521c593c6ea93dc9acdf07
Abraham May <Abrahammay1015@gmail.com>
2024-08-07 16:59:40 -0500

    Added spacing and the buttons and the text field

--- TimeCapsuleApp/PostButton.swift
+++ TimeCapsuleApp/PostButton.swift
@@ -6,13 +6,56 @@
 //
 
 import SwiftUI
+import Firebase
 
-struct PostButton: View {
+struct PostView: View {
+    @State private var selectedImage: UIImage?
+    @State private var caption: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        VStack {
+            Spacer()
+            
+            ZStack(alignment: .leading) {
+                           if caption.isEmpty {
+                               Text("Say something about this day...")
+                                   .foregroundColor(.black)
+                                   .padding(.leading, 8)
+                           }
+                           TextField("", text: $caption)
+                               .foregroundColor(.black)
+                               .padding(8)
+                       }
+                       .padding()
+            
+            Spacer()
+            
+            ZStack {
+                Rectangle()
+                    .frame(width: 291, height: 62)
+                    .cornerRadius(40)
+                    .foregroundColor(.black)
+                    .shadow(radius: 24, x: 0, y: 14)
+                
+                HStack {
+                    Text("Post")
+                        .foregroundColor(.white)
+                        .font(.system(size: 16, weight: .semibold))
+                }
+            }
+            .padding(.bottom, 20)
+        }
+    }
+
+    private func postImageWithCaption() {
+        guard let image = selectedImage else { return }
+        
+        // Here you can add code to upload the image and caption to Firebase
     }
 }
 
 #Preview {
-    PostButton()
+    PostView()
 }
