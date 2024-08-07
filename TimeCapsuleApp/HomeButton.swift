56e0ccc5a92153e0f52a73c86a797b9495d36112
Abraham May <Abrahammay1015@gmail.com>
2024-08-07 11:12:14 -0500

    Created a new file meant for the same buttons also in Home.swift

--- TimeCapsuleApp/HomeButton.swift
+++ TimeCapsuleApp/HomeButton.swift
@@ -9,10 +9,42 @@ import SwiftUI
 
 struct HomeButton: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+
+            HStack {
+                Button(action: {
+                    
+                }, label: {
+                    ZStack {
+                        Circle()
+                            .stroke(Color.gray, lineWidth: 3)
+                            .frame(width: 24, height: 24)
+                        
+                        Circle()
+                            .frame(width: 13, height: 13)
+                            .foregroundColor(.gray)
+                    }
+                })
+                
+                
+                Spacer()
+                    .frame(width: 72)
+
+                Button(action: {
+                    
+                }) {
+                    Image("Notebook")
+                        .renderingMode(.template) // Use template rendering mode
+                        .foregroundColor(.red)
+                }
+            }
+            .padding(.bottom, 20) // Adjust as needed for safe area or design requirements
+        }
     }
 }
-
-#Preview {
-    HomeButton()
+struct HomeButton_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeButton()
+    }
 }
