e706dc60baffc812acfe0fb658aa5f7c0ab8c556
Abraham May <Abrahammay1015@gmail.com>
2024-08-07 22:26:13 -0500

    Edited a few things

--- TimeCapsuleApp/Camera/CameraController.swift
+++ TimeCapsuleApp/Camera/CameraController.swift
@@ -2,9 +2,11 @@ import SwiftUI
 
 struct CameraView: UIViewControllerRepresentable {
     @Binding var navigateToHome: Bool
+    var cameraDelegate: CameraDelegate?
 
     func makeUIViewController(context: Context) -> Camera {
         let camera = Camera()
+        camera.delegate = cameraDelegate
         return camera
     }
 
@@ -16,11 +18,16 @@ struct CameraView: UIViewControllerRepresentable {
         Coordinator(self)
     }
 
-    class Coordinator: NSObject {
+    class Coordinator: NSObject, CameraDelegate {
         var parent: CameraView
 
         init(_ parent: CameraView) {
             self.parent = parent
+        }
+
+        func didTakePhoto() {
+            // Handle the photo taken event
+            parent.navigateToHome = true
         }
     }
 }
@@ -30,18 +37,52 @@ struct CameraController: View {
 
     var body: some View {
         ZStack {
-            CameraView(navigateToHome: $navigateToHome)
+            CameraView(navigateToHome: $navigateToHome, cameraDelegate: CameraController.Coordinator(self))
                 .edgesIgnoringSafeArea(.all)
             
-            VStack {
-                Spacer()
-                HomeButton()
-                    .padding(.bottom, 30) // Adjust as needed
+            if !navigateToHome {
+                VStack {
+                    Spacer()
+                    HomeButton()
+                        .padding(.bottom, 30) // Adjust as needed
+                }
             }
         }
         .navigationBarHidden(true) // Hide the navigation bar if somehow it's still shown
     }
+    
+    class Coordinator: NSObject, CameraDelegate {
+        var parent: CameraController
+
+        init(_ parent: CameraController) {
+            self.parent = parent
+        }
+
+        func didTakePhoto() {
+            // Handle the photo taken event
+            DispatchQueue.main.async {
+                self.parent.navigateToHome = true
+            }
+        }
+    }
 }
+
+
+//struct PostButton: View {
+//    var body: some View {
+//        Button(action: {
+//            // Define the action for posting the photo
+//            print("Post Button tapped!")
+//        }) {
+//            Text("Post")
+//                .font(.title)
+//                .padding()
+//                .background(Color.blue)
+//                .foregroundColor(.white)
+//                .cornerRadius(10)
+//        }
+//    }
+//}
 
 struct CameraController_Previews: PreviewProvider {
     static var previews: some View {
