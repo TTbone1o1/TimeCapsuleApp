0d0abfc3d51b16fecc3c0ba34622d84daef58ddc
Abraham May <Abrahammay1015@gmail.com>
2024-08-15 13:52:50 -0500

    Set up the Notificaitons

--- TimeCapsuleApp/Notification.swift
+++ TimeCapsuleApp/Notification.swift
@@ -1,18 +1,33 @@
-//
-//  NotificationManager.swift
-//  TimeCapsuleApp
-//
-//  Created by Abraham May on 8/15/24.
-//
+import UserNotifications
 
-import SwiftUI
+class Notification {
+    static let shared = Notification()
 
-struct NotificationManager: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    func scheduleNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Time to Post!"
+        content.body = "Hey, it's time to talk about what you did today."
+        content.sound = .default
+
+        // Change the interval to 7200 seconds for 2 hours
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 7200, // 60 seconds for testing, change to 7200 for production
+                                                        repeats: true)
+        
+        let request = UNNotificationRequest(identifier: "timeToPostNotification", // Use a fixed identifier if you want to manage notifications
+                                            content: content,
+                                            trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error scheduling notification: \(error.localizedDescription)")
+            } else {
+                print("Notification scheduled successfully.")
+            }
+        }
     }
-}
 
-#Preview {
-    NotificationManager()
+    func cancelNotifications() {
+        // Cancels notifications with the specific identifier
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["timeToPostNotification"])
+    }
 }
