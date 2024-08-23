d5e814d6fcf7d2ac7c1f997266729f56607aa8bc
Abraham May <Abrahammay1015@gmail.com>
2024-08-23 10:28:26 -0500

    Setup the signout

--- TimeCapsuleApp/Setting.swift
+++ TimeCapsuleApp/Setting.swift
@@ -1,18 +1,38 @@
-//
-//  Setting.swift
-//  TimeCapsuleApp
-//
-//  Created by Abraham May on 8/22/24.
-//
-
 import SwiftUI
+import FirebaseAuth
 
 struct Setting: View {
+    @Binding var isSignedOut: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Rectangle()
+                .frame(width: 291, height: 62)
+                .cornerRadius(40)
+                .foregroundColor(.red)
+                .shadow(radius: 24, x: 0, y: 14)
+            
+            HStack {
+                Text("Sign Out")
+                    .foregroundColor(.white)
+                    .font(.system(size: 16, weight: .semibold))
+            }
+            .onTapGesture {
+                signOut()
+            }
+        }
+    }
+    
+    private func signOut() {
+        do {
+            try Auth.auth().signOut()
+            isSignedOut = true
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
     }
 }
 
 #Preview {
-    Setting()
+    Setting(isSignedOut: .constant(false))
 }
