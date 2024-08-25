764022b459e85ab65e510654be17d0d0f0b963c2
Abraham May <Abrahammay1015@gmail.com>
2024-08-25 12:49:32 -0500

    Also added a back button

--- TimeCapsuleApp/Setting.swift
+++ TimeCapsuleApp/Setting.swift
@@ -11,8 +11,26 @@ struct Setting: View {
             Color.white.edgesIgnoringSafeArea(.all)
             
             VStack {
-                //Text("Drag offset: \(dragOffset)")
+                // Back button
+                HStack {
+                    Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Image(systemName: "chevron.left")
+                            .foregroundColor(.black)
+                            .imageScale(.large)
+                        Text("Back")
+                            .foregroundColor(.black)
+                    }
+                    .padding(.leading, 16)
+                    .padding(.top, 16)
+                    
+                    Spacer()
+                }
                 
+                Spacer()
+                
+                // Your existing sign out button
                 Rectangle()
                     .frame(width: 291, height: 62)
                     .cornerRadius(40)
@@ -26,6 +44,8 @@ struct Setting: View {
                     .onTapGesture {
                         signOut()
                     }
+                
+                Spacer()
             }
         }
         .gesture(
@@ -35,13 +55,12 @@ struct Setting: View {
                 }
                 .onEnded { gesture in
                     if gesture.translation.width > 100 {
-                        print("Attempting to dismiss")
                         self.presentationMode.wrappedValue.dismiss()
                     }
                     self.dragOffset = 0
                 }
         )
-        .navigationBarBackButtonHidden(true)
+        .navigationBarHidden(true)
     }
     
     private func signOut() {
@@ -52,8 +71,4 @@ struct Setting: View {
             print("Error signing out: %@", signOutError)
         }
     }
-}
-
-#Preview {
-    Setting(isSignedOut: .constant(false))
 }
