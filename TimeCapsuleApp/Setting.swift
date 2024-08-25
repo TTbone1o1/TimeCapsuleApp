d3068a4e2e2f0ac11dcc8cb325997a688f3be097
Abraham May <Abrahammay1015@gmail.com>
2024-08-25 12:04:43 -0500

    Added a swipe gesture to go back to homescreen

--- TimeCapsuleApp/Setting.swift
+++ TimeCapsuleApp/Setting.swift
@@ -3,24 +3,45 @@ import FirebaseAuth
 
 struct Setting: View {
     @Binding var isSignedOut: Bool
+    @Environment(\.presentationMode) var presentationMode
+    @State private var dragOffset: CGFloat = 0
     
     var body: some View {
         ZStack {
-            Rectangle()
-                .frame(width: 291, height: 62)
-                .cornerRadius(40)
-                .foregroundColor(.red)
-                .shadow(radius: 24, x: 0, y: 14)
+            Color.white.edgesIgnoringSafeArea(.all)
             
-            HStack {
-                Text("Sign Out")
-                    .foregroundColor(.white)
-                    .font(.system(size: 16, weight: .semibold))
-            }
-            .onTapGesture {
-                signOut()
+            VStack {
+                //Text("Drag offset: \(dragOffset)")
+                
+                Rectangle()
+                    .frame(width: 291, height: 62)
+                    .cornerRadius(40)
+                    .foregroundColor(.red)
+                    .shadow(radius: 24, x: 0, y: 14)
+                    .overlay(
+                        Text("Sign Out")
+                            .foregroundColor(.white)
+                            .font(.system(size: 16, weight: .semibold))
+                    )
+                    .onTapGesture {
+                        signOut()
+                    }
             }
         }
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    self.dragOffset = gesture.translation.width
+                }
+                .onEnded { gesture in
+                    if gesture.translation.width > 100 {
+                        print("Attempting to dismiss")
+                        self.presentationMode.wrappedValue.dismiss()
+                    }
+                    self.dragOffset = 0
+                }
+        )
+        .navigationBarBackButtonHidden(true)
     }
     
     private func signOut() {
