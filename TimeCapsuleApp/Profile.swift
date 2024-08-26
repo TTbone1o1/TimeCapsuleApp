72bb65256382f62e22e5ec5563337f1d0a86bc8b
Abraham May <Abrahammay1015@gmail.com>
2024-08-26 18:22:10 -0500

    Edited profile

--- TimeCapsuleApp/Profile.swift
+++ TimeCapsuleApp/Profile.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct Profile: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+                .frame(height: 128) // Create a spacer with a height of 128
+
+            ZStack {
+                Circle()
+                    .stroke(Color(.systemGray6), lineWidth: 6) // Stroke with width of 6 and gray color
+                    .frame(width: 148, height: 148)
+                    
+                Circle()
+                    .frame(width: 125, height: 125)
+            }
+            Spacer()
+                .frame(height: 20)
+            Text("Abraham May")
+                .font(.system(size: 32, weight: .bold, design: .rounded))
+                .fontWeight(.bold)
+                .padding(.leading)
+                .foregroundColor(Color.primary)
+
+            Spacer() // Optional: This will center the circle vertically if more space is available
+        }
+        .edgesIgnoringSafeArea(.top) // Ignore the safe area to ensure spacing from the very top of the screen
     }
 }
 
