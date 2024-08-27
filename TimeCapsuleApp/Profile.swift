622deda974609dc2c5a6ba9e4d5ec01c443f9aea
Abraham May <Abrahammay1015@gmail.com>
2024-08-27 14:38:49 -0500

    Fixed the issue where signing out didnt take you to timecap now it does

--- TimeCapsuleApp/Profile.swift
+++ TimeCapsuleApp/Profile.swift
@@ -5,63 +5,80 @@ struct Profile: View {
     @State private var displayedMonth = Calendar.current.component(.month, from: Date())
     @State private var displayedYear = Calendar.current.component(.year, from: Date())
     @State private var isShowingSetting = false
+    @State private var isSignedOut = false
     @Environment(\.presentationMode) var presentationMode
+    @State private var navigateToTimeCap = false
 
     var body: some View {
-        ZStack {
-            VStack {
-                Spacer()
-                    .frame(height: 128) // Create a spacer with a height of 128
+        NavigationView {
+            ZStack {
+                GeometryReader { geometry in
+                    VStack {
+                        Spacer()
+                            .frame(height: 128) // Create a spacer with a height of 128
 
-                ZStack {
-                    Circle()
-                        .stroke(Color(.systemGray6), lineWidth: 6) // Stroke with width of 6 and gray color
-                        .frame(width: 148, height: 148)
-                    
-                    Circle()
-                        .frame(width: 125, height: 125)
-                        .onTapGesture {
-                            withAnimation {
-                                isShowingSetting.toggle()
+                        ZStack {
+                            Circle()
+                                .stroke(Color(.systemGray6), lineWidth: 6) // Stroke with width of 6 and gray color
+                                .frame(width: 148, height: 148)
+                            
+                            Circle()
+                                .frame(width: 125, height: 125)
+                                .onTapGesture {
+                                    withAnimation {
+                                        isShowingSetting.toggle()
+                                    }
+                                }
+                        }
+                        Spacer()
+                            .frame(height: 20)
+                        Text("Abraham May")
+                            .font(.system(size: 32, weight: .bold, design: .rounded))
+                            .fontWeight(.bold)
+                            .padding(.leading)
+                            .foregroundColor(Color.primary)
+
+                        CalendarView(currentDate: $currentDate, displayedMonth: $displayedMonth, displayedYear: $displayedYear)
+
+                        Spacer() // Optional: This will center the circle vertically if more space is available
+                    }
+                    .edgesIgnoringSafeArea(.top) // Ignore the safe area to ensure spacing from the very top of the screen
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                print("Gesture in progress: \(value.translation.width)")
                             }
-                        }
+                            .onEnded { value in
+                                print("Gesture ended: \(value.translation.width)")
+                                if value.translation.width > 100 {
+                                    self.presentationMode.wrappedValue.dismiss()
+                                }
+                            }
+                    )
+                    .onTapGesture {
+                        // Dismiss view when tapped anywhere on the screen
+                        self.presentationMode.wrappedValue.dismiss()
+                    }
+                    .background(Color.clear) // Make sure the background is clear so gestures are recognized
                 }
-                Spacer()
-                    .frame(height: 20)
-                Text("Abraham May")
-                    .font(.system(size: 32, weight: .bold, design: .rounded))
-                    .fontWeight(.bold)
-                    .padding(.leading)
-                    .foregroundColor(Color.primary)
-
-                // Calendar View
-                CalendarView(currentDate: $currentDate, displayedMonth: $displayedMonth, displayedYear: $displayedYear)
-
-                Spacer() // Optional: This will center the circle vertically if more space is available
+                
+                if isShowingSetting {
+                    Setting(isShowing: $isShowingSetting, isSignedOut: $isSignedOut)
+                        .transition(.move(edge: .bottom))
+                        .animation(.easeInOut)
+                        .zIndex(1) // Make sure Setting has a higher zIndex than the Profile content
+                }
+                
+                if navigateToTimeCap {
+                    Timecap() // Replace with your actual TimeCap view
+                        .transition(.opacity)
+                        .zIndex(1) // Ensure it appears above other content
+                }
             }
-            .edgesIgnoringSafeArea(.top) // Ignore the safe area to ensure spacing from the very top of the screen
-            .gesture(
-                DragGesture()
-                    .onChanged { value in
-                        print("Gesture in progress: \(value.translation.width)")
-                    }
-                    .onEnded { value in
-                        print("Gesture ended: \(value.translation.width)")
-                        if value.translation.width > 100 {
-                            self.presentationMode.wrappedValue.dismiss()
-                        }
-                    }
-            )
-            .onTapGesture {
-                // Dismiss view when tapped anywhere on the screen
-                self.presentationMode.wrappedValue.dismiss()
-            }
-
-            if isShowingSetting {
-                Setting(isSignedOut: .constant(false), isShowing: $isShowingSetting)
-                    .transition(.move(edge: .bottom))
-                    .animation(.easeInOut)
-                    .zIndex(1) // Make sure Setting has a higher zIndex than the Profile content
+            .onChange(of: isSignedOut) { signedOut in
+                if signedOut {
+                    navigateToTimeCap = true
+                }
             }
         }
     }
@@ -109,15 +126,11 @@ struct CalendarView: View {
             let columns = Array(repeating: GridItem(.flexible()), count: 7)
             
             LazyVGrid(columns: columns, spacing: 10) {
-                ForEach(0..<daysInMonth + firstWeekday - 1, id: \.self) { i in
-                    if i >= firstWeekday - 1 {
-                        let day = i - firstWeekday + 2
-                        Text("\(day)")
-                            .font(.system(size: 18, weight: .bold, design: .rounded))
-                            .foregroundColor(day == currentDay() ? .black : .gray)
-                    } else {
-                        Text("")
-                    }
+                ForEach((0..<daysInMonth + firstWeekday - 1).filter { $0 >= firstWeekday - 1 }, id: \.self) { i in
+                    let day = i - firstWeekday + 2
+                    Text("\(day)")
+                        .font(.system(size: 18, weight: .bold, design: .rounded))
+                        .foregroundColor(day == currentDay() ? .black : .gray)
                 }
             }
         }
@@ -146,8 +159,4 @@ struct CalendarView: View {
             currentDate = newDate
         }
     }
-}
-
-#Preview {
-    Profile()
 }
