81af4a7c4c2a9c43d4273df4826818ebf570918b
Abraham May <Abrahammay1015@gmail.com>
2024-08-27 11:13:46 -0500

    got rid of the notification

--- TimeCapsuleApp/TimeCapsuleApp.swift
+++ TimeCapsuleApp/TimeCapsuleApp.swift
@@ -6,20 +6,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         FirebaseApp.configure()
-        requestNotificationPermission()
         return true
-    }
-
-    private func requestNotificationPermission() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-            if let error = error {
-                print("Error requesting notification permission: \(error.localizedDescription)")
-            } else if granted {
-                print("Notification permission granted.")
-            } else {
-                print("Notification permission denied.")
-            }
-        }
     }
 }
 
